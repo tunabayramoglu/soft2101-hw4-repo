4d794bd7ad6a73adc2b889fea7cb4cc6e6385c13
Tuna Bayramoğlu <tunabayram34@gmail.com>
2025-12-31 17:14:57 +0300

    satifying requirement one

---
+++ hmk1/RangePrinter.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class RangePrinter{
+	public static void main(String[] args){
+		Scanner scn = new Scanner(System.in);
+
+		System.out.println("Enter first number");
+		int number1 = scn.nextInt();
+
+		System.out.println("Enter second number");
+		int number2 = scn.nextInt();
+
+		int start = Math.min(number1, number2);
+		int end = Math.max(number1, number2);
+
+		System.out.print("Numbers between " + start + " and " + end + ": ");
+		for(int i = start + 1 ; i < end; i++){
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+}
